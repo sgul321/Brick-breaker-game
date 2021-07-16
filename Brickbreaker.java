053e4e6eda8f1bc881d4d6fbe6b70f4bd41ba4b7
sgul321 <87537999+sgul321@users.noreply.github.com>
2021-07-16 21:02:27 +0500

    Add files via upload

---
+++ Brickbreaker.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package brickbreaker;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author DELL
+ */
+public class Brickbreaker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame obj = new JFrame();
+        Gameplay gameplay = new Gameplay();
+        obj.setBounds(10, 10, 700, 600);
+        obj.setTitle("Breakout Ball");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gameplay);
+        
+                
+        
+        
+        
+    }
+    
+}
---
+++ MpGenerator.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package brickbreaker;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+
+public class MpGenerator {
+    public int map[][];
+    public int brickwidth;
+    public int brickHeight;
+    public MpGenerator(int row, int col){
+        map = new int[row][col];
+        for(int i = 0;i < map.length;i++){
+            for(int j = 0; j< map[0].length; j++){
+                map[i][j] = 1;
+                
+            }
+            
+        }
+        brickwidth = 540/col;
+        brickHeight = 150/row;
+    }
+    public void draw(Graphics2D g){
+        for(int i = 0;i < map.length;i++){
+            for(int j = 0; j< map[0].length; j++){
+                if(map[i][j] > 0){
+                    g.setColor(Color.white);
+                    g.fillRect(j * brickwidth + 80, i * brickHeight + 50, brickwidth, brickHeight);
+                    
+                    g.setStroke(new BasicStroke(3));
+                    g.setColor(Color.black);
+                    g.drawRect(j * brickwidth + 80, i * brickHeight + 50, brickwidth, brickHeight);
+                    
+                }
+            }
+        
+        
+        
+    }
+  }
+    public void setBrickvalue(int value, int row, int col){
+        map[row][col] = value;
+        
+    }
+}
+
+    
+
